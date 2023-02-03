a6b27758409861788c60f2cf3de02fe29d9d3f51
chrissyast <cjastles@gmail.com>
2023-02-03 00:32:03 +0000

    create job and test against example files

---
+++ app/jobs/parse_file_job.rb
@@ -0,0 +1,37 @@
+class ParseFileJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    upload = ::Upload.new()
+    upload.save
+    file = File.open("app/assets/demo-models-pack/model-2.inp")
+    blocks = file.read.split("..")
+    blocks.each do |b|
+      type = ""
+      args = {}
+      args[:upload_id] = upload.id
+      lines = b.split("\n").select {|l| l[0] != "$" and l.include? "="}
+      lines.each_with_index do |l, i|
+        pair = l.split("=").map do |l|
+          l = l.strip
+          l = l.gsub("\"", "")
+        end
+        if i == 0 # header information
+          args[:name] = pair[0]
+          type = pair[1].split("-").map(&:capitalize).join
+        elsif pair[0] and pair[0] != "" # all other properties
+          key = pair[0].downcase.gsub("-", "_") # put it into rails-style casing
+          key = "x_type" if key == "type" # 'type' is a reserved word
+          value = pair[1]
+          args[key] = value
+          end
+
+      end
+      if type.length > 0
+        object_record = "#{type}".constantize.new(args)
+        object_record.save
+      end
+    end
+    return "done"
+  end
+end
