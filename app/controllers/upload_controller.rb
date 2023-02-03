b029d1b9f6e4d8527b741f810e741a3db1cd8129
chrissyast <cjastles@gmail.com>
2023-02-03 01:36:26 +0000

    redirect to 'show' page on starting job

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -3,8 +3,12 @@ class UploadController < ApplicationController
     upload = Upload.create()
     file = request.body
     ParseFileJob.perform_now(file, upload.id)
+    redirect_to(:action => "show", :id => upload.id)
   end
 
   def new
+  end
+
+  def show
   end
 end
