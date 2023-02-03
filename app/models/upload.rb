6cef206f31ad934374814db749edf8ddf89927bb
chrissyast <cjastles@gmail.com>
2023-02-03 22:28:28 +0000

    render file in view

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,14 @@
 class Upload < ApplicationRecord
+  has_one :general
+  has_many :compliances
+  has_many :constructions
+  has_many :dhw_generators
+  has_many :doors
+  has_many :glasses
+  has_many :hvac_systems
+  has_many :rec_projects
+  has_many :rec_users
+  has_many :walls
+  has_many :windows
+  has_many :zones
 end
